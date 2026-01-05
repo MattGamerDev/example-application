6521c41a074e2b942085741a8c17a5f98c7426ad
LAPTOP-MAA41VL3\mtthw <mtthwgamb@gmail.com>
2026-01-05 09:44:35 -0700

    Version 4.0 - broken

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'v4.0.0 - BROKEN VERSION!'  # Pretend this is broken
+    return 'v4.0.0 - BROKEN VERSION   !!'  # Pretend this is broken
 
 app.run(host='0.0.0.0', port=8080)
